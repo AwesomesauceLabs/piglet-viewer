931b921ff493581e7d05e59c7a7705f9ffd931e0
Ben Vandervalk <ben.vvalk@gmail.com>
2019-10-04 19:48:55 -0500

    add: GLTFImporter (new parent class of runtime & editor importers)
    Formerly, `GLTFRuntimeImporter` was the parent class of `GLTFEditorImporter`,
    but now I am introducing `GLTFImporter` as a common parent class of
    `GLTFRuntimeImporter` and `GLTFEditorImporter`.  Most of the importing code
    now lives in `GLTFImporter`.
    
    I wanted to make `GLTFRuntimeImporter` a child of `GLTFImporter` so that I could
    specialize the constructor of `GLTFRuntimeImporter` and add the build-time
    shader configuration as a parameter. This will enable me to check what shaders
    variants/keywords are available at runtime and to catch errors while I'm
    importing materials.

--- Assets/UnityGLTF/Scripts/GLTFEditorImporter.cs
+++ Assets/UnityGLTF/Scripts/GLTFEditorImporter.cs
@@ -17,7 +17,7 @@ namespace UnityGLTF
 	/// Editor windows to load a GLTF scene in editor
 	/// </summary>
 	///
-	public class GLTFEditorImporter : GLTFRuntimeImporter
+	public class GLTFEditorImporter : GLTFImporter
 	{
 		// Import paths and options
 		/// <summary>
