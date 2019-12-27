b6b94749da36e4fa185084862fe0604919f37ec1
Ben Vandervalk <ben.vvalk@gmail.com>
2019-12-27 11:07:43 -0600

    ViewerGUI: reposition "Spin X" / "Spin Y", fix GUIStyle inits

--- Assets/PigletViewer/Scripts/ViewerGUI.cs
+++ Assets/PigletViewer/Scripts/ViewerGUI.cs
@@ -27,9 +27,21 @@ public class ViewerGUI
     /// current glTF import.
     /// </summary>
     public List<string> Log;
+
+    private class Styles
+    {
+        public GUIStyle Title;
+        public GUIStyle Text;
+        public GUIStyle SliderLabel;
+        public GUIStyle FooterText;
+    }
+
+    private Styles _styles;
     
     public ViewerGUI()
     {
+        _styles = null;
+        
         Reset();
     }
 
@@ -55,85 +67,124 @@ public class ViewerGUI
         ResetLog();
         ResetFooterMessage();
     }
-    
+
+    private void InitStyles()
+    {
+        if (_styles != null)
+            return;
+
+        _styles = new Styles();
+        
+        _styles.Title = new GUIStyle(GUI.skin.label);
+        _styles.Title.alignment = TextAnchor.MiddleLeft;
+        _styles.Title.margin = new RectOffset(
+            _styles.Title.margin.left, 0, 20, 20);
+        _styles.Title.fontSize = 24;
+        _styles.Title.fontStyle = FontStyle.Bold;
+
+        _styles.Text = new GUIStyle(GUI.skin.label);
+        _styles.Text.alignment = TextAnchor.MiddleLeft;
+        _styles.Text.fontStyle = FontStyle.Normal;
+        _styles.Text.padding = new RectOffset(0, 0, 0, 0);
+        _styles.Text.fontSize = 18;
+
+        _styles.SliderLabel = new GUIStyle(GUI.skin.label);
+        _styles.SliderLabel.alignment = TextAnchor.MiddleCenter;
+        _styles.SliderLabel.fontSize = 18;
+        
+        _styles.FooterText = new GUIStyle(GUI.skin.label);
+        _styles.FooterText.alignment = TextAnchor.MiddleCenter;
+        _styles.FooterText.fontStyle = FontStyle.Italic;
+        _styles.FooterText.fontSize = 24;
+    }
+
     /// <summary>
     /// Draws GUI elements (e.g. progress messages, checkboxes)
     /// onto the model viewer window.
     /// </summary>
     public void OnGUI()
     {
+        InitStyles();
+        
         // set font color to black
         GUI.contentColor = Color.black;
 
-        var labelStyle = GUI.skin.GetStyle("Label");
-        labelStyle.alignment = TextAnchor.MiddleLeft;
-        labelStyle.fontStyle = FontStyle.Normal;
-        labelStyle.fontSize = 18;
+        float padding = 25;
 
-        float padding = 20;
-        
         GUILayout.BeginArea(new Rect(
             padding, padding,
             Screen.width - 2 * padding,
             Screen.height - 2 * padding));
+        
+            // progress log messages
+            foreach (var line in Log)
+               GUILayout.Label(line, _styles.Text);
+        
+        GUILayout.EndArea();
 
-        float rowHeight = 20;
+        // footer message and "Spin X" / "Spin Y" sliders
+        
+        float footerAreaHeight = 100;
+        
         float labelWidth = 100;
         float sliderWidth = 100;
+        float sliderAreaWidth = 2 * labelWidth + 2 * sliderWidth;
+
+        Rect footerAreaRect = new Rect(
+            0, Screen.height - footerAreaHeight,
+            Screen.width, footerAreaHeight);
         
-        GUILayout.BeginVertical(GUILayout.Height(rowHeight));
+        GUILayout.BeginArea(footerAreaRect);
+
+            GUILayout.FlexibleSpace();
+            
             GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                GUILayout.Label(FooterMessage, _styles.FooterText);
+                GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            
+            GUILayout.FlexibleSpace();
+            
+            GUILayout.BeginHorizontal();
+            
+                GUILayout.FlexibleSpace();
+                
                 GUILayout.BeginVertical();
                     GUILayout.FlexibleSpace();
-                    GUILayout.Label("Spin X", labelStyle,
+                    GUILayout.Label("Spin X", _styles.SliderLabel,
                         GUILayout.Width(labelWidth));
                     GUILayout.FlexibleSpace();
                 GUILayout.EndVertical();
+                
                 GUILayout.BeginVertical();
                     GUILayout.FlexibleSpace();
                     SpinX = GUILayout.HorizontalSlider(
                         SpinX, 0, 1, GUILayout.Width(sliderWidth));
                     GUILayout.FlexibleSpace();
                 GUILayout.EndVertical();
-                GUILayout.FlexibleSpace();
-            GUILayout.EndHorizontal();
-        GUILayout.EndVertical();
-        
-        GUILayout.BeginVertical(GUILayout.Height(rowHeight));
-            GUILayout.BeginHorizontal();
+                
                 GUILayout.BeginVertical();
                     GUILayout.FlexibleSpace();
-                    GUILayout.Label("Spin Y", labelStyle,
+                    GUILayout.Label("Spin Y", _styles.SliderLabel,
                         GUILayout.Width(labelWidth));
                     GUILayout.FlexibleSpace();
                 GUILayout.EndVertical();
+                
                 GUILayout.BeginVertical();
                     GUILayout.FlexibleSpace();
                     SpinY = GUILayout.HorizontalSlider(
                         SpinY, 0, 1, GUILayout.Width(sliderWidth));
                     GUILayout.FlexibleSpace();
                 GUILayout.EndVertical();
+                
                 GUILayout.FlexibleSpace();
+                
             GUILayout.EndHorizontal();
-        GUILayout.EndVertical();
-        
-        GUILayout.Space(30);
-        
-        // progress log messages
-        
-        foreach (var line in Log)
-            GUILayout.Label(line, labelStyle);
-        
+            
+            GUILayout.FlexibleSpace();
+
         GUILayout.EndArea();
         
-        // status message along the bottom of the window
-        
-        var footerStyle = GUI.skin.GetStyle("Label");
-        footerStyle.alignment = TextAnchor.MiddleCenter;
-        footerStyle.fontStyle = FontStyle.Italic;
-        footerStyle.fontSize = 22;
-
-        GUI.Label(new Rect(0, Screen.height - 100, Screen.width, 100),
-            FooterMessage, footerStyle);
     }
 }
