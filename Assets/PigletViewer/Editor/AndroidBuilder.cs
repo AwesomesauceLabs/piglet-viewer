41e48884ecf0edee9e331272795c70861398da7d
Ben Vandervalk <ben.vvalk@gmail.com>
2020-02-26 10:00:44 -0500

    Builds/Android: add apk.do script to run Android build [new]
    The apk.do runs an Android build from the command line and generates
    Builds/Android/piglet.apk.
    
    To run the script, do:
    
    $ redo apk

---
+++ Assets/PigletViewer/Editor/AndroidBuilder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class AndroidBuilder
+{
+    static void Build() {
+        string[] scenes = {"Assets/PigletViewer/Scenes/MainScene.unity"};
+        BuildPipeline.BuildPlayer(scenes, "Builds/Android/piglet.apk",
+            BuildTarget.Android, BuildOptions.None);
+    }
+}
