511aa9676669e5232f168f268e71f504fc6763b3
Ben Vandervalk <ben.vvalk@gmail.com>
2020-04-19 13:39:46 -0400

    WebGL: fix build error due to recent refactoring [buildfix]
    JsLib -> PigletJsLib / PigletViewerJsLib

--- Assets/PigletViewer/Scripts/ImportLog.cs
+++ Assets/PigletViewer/Scripts/ImportLog.cs
@@ -75,7 +75,7 @@ namespace Piglet
         public void AddLine(string line)
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
-            JsLib.AppendLogLine(line);
+            PigletViewerJsLib.AppendLogLine(line);
 #else
             Lines.Add(line);
 #endif
@@ -87,7 +87,7 @@ namespace Piglet
         public void ReplaceLastLine(string line)
         {
 #if UNITY_WEBGL && !UNITY_EDITOR
-            JsLib.UpdateTailLogLine(line);
+            PigletViewerJsLib.UpdateTailLogLine(line);
 #else
             Lines[Lines.Count - 1] = line;
 #endif
