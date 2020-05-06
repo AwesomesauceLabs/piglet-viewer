bd9053495b29f40c09b1dae5196ea27caa18eff1
Ben Vandervalk <ben.vvalk@gmail.com>
2020-05-06 10:23:27 -0400

    AndroidBuilder: add comment with example command line invocation [doc]

--- Assets/PigletViewer/Editor/AndroidBuilder.cs
+++ Assets/PigletViewer/Editor/AndroidBuilder.cs
@@ -2,6 +2,15 @@
 
 namespace PigletViewer
 {
+    /// <summary>
+    /// This class enables Android .apk builds from the command line,
+    /// by running:
+    ///
+    /// `$ Unity.exe -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget Android -executeMethod AndroidBuilder.Build`
+    ///
+    /// Note: This script needs to be placed under an `Editor` folder under
+    /// `Assets`.
+    /// </summary>
     public class AndroidBuilder
     {
         static void Build() {
