95be87ccdc57f3616d402e873d5ad34946df8048
Ben Vandervalk <ben.vvalk@gmail.com>
2020-05-07 12:29:30 -0400

    move PigletViewerJsLib to PigletViewer namespace [refactor]

--- Assets/PigletViewer/Scripts/WebGL/PigletViewerJsLib.cs
+++ Assets/PigletViewer/Scripts/WebGL/PigletViewerJsLib.cs
@@ -3,67 +3,71 @@ using System;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-/// <summary>
-/// Javascript methods that are callable from C#.
-///
-/// The definitions for these functions are in PigletViewer.jslib.
-/// </summary>
-public static class PigletViewerJsLib
+namespace PigletViewer
 {
     /// <summary>
-    /// Performs one-time Javascript initialization
-    /// (e.g. setting up event handlers).
+    /// Javascript methods that are callable from C#.
+    ///
+    /// The definitions for these functions are in PigletViewer.jslib.
     /// </summary>
-    [DllImport("__Internal")]
-    public static extern void Init();
+    public static class PigletViewerJsLib
+    {
+        /// <summary>
+        /// Performs one-time Javascript initialization
+        /// (e.g. setting up event handlers).
+        /// </summary>
+        [DllImport("__Internal")]
+        public static extern void Init();
 
-    /// <summary>
-    /// Get the size of a file in bytes.
-    /// </summary>
-    /// <param name="filename">
-    /// The basename for the file that was last selected
-    /// by the user.
-    /// </param>
-    /// <returns></returns>
-    [DllImport("__Internal")]
-    public static extern int GetFileSize(string filename);
+        /// <summary>
+        /// Get the size of a file in bytes.
+        /// </summary>
+        /// <param name="filename">
+        /// The basename for the file that was last selected
+        /// by the user.
+        /// </param>
+        /// <returns></returns>
+        [DllImport("__Internal")]
+        public static extern int GetFileSize(string filename);
 
-    /// <summary>
-    /// Get the contents of a file as byte array.
-    /// </summary>
-    /// <param name="filename">
-    /// The basename for the file that was last selected
-    /// by the user.
-    /// </param>
-    /// <returns>
-    /// A native pointer to a byte array containing the
-    /// contents of the file.
-    /// </returns>
-    [DllImport("__Internal")]
-    public static extern IntPtr GetFileData(string filename);
+        /// <summary>
+        /// Get the contents of a file as byte array.
+        /// </summary>
+        /// <param name="filename">
+        /// The basename for the file that was last selected
+        /// by the user.
+        /// </param>
+        /// <returns>
+        /// A native pointer to a byte array containing the
+        /// contents of the file.
+        /// </returns>
+        [DllImport("__Internal")]
+        public static extern IntPtr GetFileData(string filename);
 
-    /// <summary>
-    /// Free the contents of a file from Javascript memory.
-    /// </summary>
-    /// <param name="filename">
-    /// The basename for the file that was last selected
-    /// by the user.
-    /// </param>
-    [DllImport("__Internal")]
-    public static extern void FreeFileData(string filename);
+        /// <summary>
+        /// Free the contents of a file from Javascript memory.
+        /// </summary>
+        /// <param name="filename">
+        /// The basename for the file that was last selected
+        /// by the user.
+        /// </param>
+        [DllImport("__Internal")]
+        public static extern void FreeFileData(string filename);
 
-    /// <summary>
-    /// Append a line to the Import Log, located in the left panel
-    /// of the web page.
-    /// </summary>
-    [DllImport("__Internal")]
-    public static extern void AppendLogLine(string line);
+        /// <summary>
+        /// Append a line to the Import Log, located in the left panel
+        /// of the web page.
+        /// </summary>
+        [DllImport("__Internal")]
+        public static extern void AppendLogLine(string line);
 
-    /// <summary>
-    /// Replace the last line of the Import Log, located in the
-    /// left panel of the web page.
-    /// </summary>
-    [DllImport("__Internal")]
-    public static extern void UpdateTailLogLine(string line);
+        /// <summary>
+        /// Replace the last line of the Import Log, located in the
+        /// left panel of the web page.
+        /// </summary>
+        [DllImport("__Internal")]
+        public static extern void UpdateTailLogLine(string line);
+    }
 }
+
 #endif
