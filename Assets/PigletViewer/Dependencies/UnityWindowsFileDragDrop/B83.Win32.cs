098cf1f9b3867e590d3907a798b940118343786e
Ben Vandervalk <ben.vvalk@gmail.com>
2020-05-13 14:09:49 -0400

    B83.Win32.cs: remove trailing whitespace [cleanup]

--- Assets/PigletViewer/Dependencies/UnityWindowsFileDragDrop/B83.Win32.cs
+++ Assets/PigletViewer/Dependencies/UnityWindowsFileDragDrop/B83.Win32.cs
@@ -1,23 +1,22 @@
-﻿/* * * * *
 ﻿#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
 /* * * * *
  * This is a collection of Win API helpers. Mainly dealing with window message hooks
  * and file drag&drop support for Windows standalone Unity applications.
- * 
+ *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2018 Markus Göbel (Bunny83)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,7 +24,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  * * * * */
 using System;
 using System.Collections.Generic;
