908cc9db5832007ecb8f3a2ed8df094eb568b58f
Ben Vandervalk <ben.vvalk@gmail.com>
2021-12-31 13:26:28 -0500

    add ColorSpaceSwitcher.cs [new]
    Editor script to automate switching between Linear and Gamma
    modes (for command-line builds).

---
+++ Assets/PigletViewer/Editor/ColorSpaceSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PigletViewer
+{
+    /// <summary>
+    /// Methods to change the current rendering mode to either
+    /// Linear or Gamma.
+    /// </summary>
+    public class ColorSpaceSwitcher
+    {
+        public static void SwitchToLinear()
+        {
+            PlayerSettings.colorSpace = ColorSpace.Linear;
+        }
+
+        public static void SwitchToGamma()
+        {
+            PlayerSettings.colorSpace = ColorSpace.Gamma;
+        }
+    }
+}
