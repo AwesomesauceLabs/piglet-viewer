97b09c3fc812b562c4f19baa6a95213fd377ec71
Ben Vandervalk <ben.vvalk@gmail.com>
2022-01-03 14:05:35 -0500

    ColorSpaceSwitcher: configure graphics APIs when switching modes [bugfix]
    As noted by a warning in the Player Settings UI, using Linear mode on
    Android and WebGL requires setting the Graphics API to OpenGLES3. (For
    WebGL, the OpenGLES3 option is displayed as "WebGL 2.0".)

--- Assets/PigletViewer/Editor/ColorSpaceSwitcher.cs
+++ Assets/PigletViewer/Editor/ColorSpaceSwitcher.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace PigletViewer
 {
@@ -12,11 +13,38 @@ namespace PigletViewer
         public static void SwitchToLinear()
         {
             PlayerSettings.colorSpace = ColorSpace.Linear;
+
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneWindows, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneWindows64, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneOSX, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneLinux64, true);
+
+            // On Android, using linear rendering mode requires
+            // setting the Graphics API to OpenGLES3.
+
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.Android, false);
+            PlayerSettings.SetGraphicsAPIs(BuildTarget.Android, new [] { GraphicsDeviceType.OpenGLES3 });
+
+            // On WebGL, using linear rendering mode requires
+            // setting the Graphics API to OpenGLES3, which corresponds
+            // to the "WebGL 2.0" option in the UI.
+
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.WebGL, false);
+            PlayerSettings.SetGraphicsAPIs(BuildTarget.WebGL, new [] { GraphicsDeviceType.OpenGLES3 });
         }
 
         public static void SwitchToGamma()
         {
             PlayerSettings.colorSpace = ColorSpace.Gamma;
+
+            // Enable "Auto Graphics API" on all platforms.
+
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneWindows, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneWindows64, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneOSX, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneLinux64, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.Android, true);
+            PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.WebGL, true);
         }
     }
 }
