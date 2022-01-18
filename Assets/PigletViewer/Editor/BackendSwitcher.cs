43efd44435534be233aef21e79c60006bc31851f
Ben Vandervalk <ben.vvalk@gmail.com>
2022-01-18 13:14:27 -0500

    add BackendSwitcher.cs [import]
    Class to switch scripting backend to IL2CPP during command-line
    builds.

---
+++ Assets/PigletViewer/Editor/BackendSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PigletViewer
+{
+    /// <summary>
+    /// Methods to change the current rendering mode to either
+    /// Linear or Gamma.
+    /// </summary>
+    public class BackendSwitcher
+    {
+        public static void SwitchToIL2CPP()
+        {
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
+            PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+        }
+    }
+}
