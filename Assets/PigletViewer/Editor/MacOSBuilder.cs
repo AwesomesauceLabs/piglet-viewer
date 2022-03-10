47b8d97bd84bef14f483dbbad4c5fbb872c88d40
Ben Vandervalk <ben.vvalk@gmail.com>
2022-03-10 10:17:49 -0500

    add MacOSBuilder.cs to perform command-line macOS builds [new]

---
+++ Assets/PigletViewer/Editor/MacOSBuilder.cs
@@ -0,0 +1,49 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace PigletViewer
+{
+    public class MacOSBuilder
+    {
+        /// <summary>
+        /// <para>
+        /// Method to run an automated macOS build from the command line.
+        /// The build output is a macOS app bundle located at
+        /// `build/macos.app` under the project root, and the output executable
+        /// is `build/macos.app/Contents/MacOS/piglet-viewer`.
+        /// </para>
+        /// <para>
+        /// To run the build from the command line, switch to the
+        /// project root folder and run:
+        /// </para>
+        /// <para>
+        /// Unity.exe -logFile /dev/stdout -executeMethod
+        /// PigletViewer.MacOSBuilder.Build -batchmode -quit
+        /// </para>
+        /// </summary>
+        [MenuItem("PigletViewer/Build/macOS")]
+        public static void Build()
+        {
+            const string buildPath = "build/macos";
+
+            // Configure the name of the output executable in
+            // `build/macos.app/Contents/MacOS`.
+
+            PlayerSettings.productName = "piglet-viewer";
+
+            // Run the build. (Generates macOS app bundle in `build/macos.app`
+            // under project root).
+
+            var buildOptions = new BuildPlayerOptions
+            {
+                target = BuildTarget.StandaloneOSX,
+                locationPathName = buildPath,
+                scenes = new [] { "Assets/PigletViewer/Scenes/MainScene.unity" }
+            };
+
+            BuildPipeline.BuildPlayer(buildOptions);
+
+            Debug.Log($"Successfully built macOS app bundle at {buildPath}!");
+        }
+    }
+}
