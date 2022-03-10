3fd1015728ab31c10819a7a129172054c7c381b5
Ben Vandervalk <ben.vvalk@gmail.com>
2022-03-10 13:11:44 -0500

    AndroidBuilder.cs: update code/comments to match other builders [refactor]

--- Assets/PigletViewer/Editor/AndroidBuilder.cs
+++ Assets/PigletViewer/Editor/AndroidBuilder.cs
@@ -1,22 +1,48 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
 namespace PigletViewer
 {
-    /// <summary>
-    /// This class enables Android .apk builds from the command line,
-    /// by running:
-    ///
-    /// `$ Unity.exe -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget Android -executeMethod AndroidBuilder.Build`
-    ///
-    /// Note: This script needs to be placed under an `Editor` folder under
-    /// `Assets`.
-    /// </summary>
     public class AndroidBuilder
     {
-        static void Build() {
-            string[] scenes = {"Assets/PigletViewer/Scenes/MainScene.unity"};
-            BuildPipeline.BuildPlayer(scenes, "Builds/Android/piglet.apk",
-                BuildTarget.Android, BuildOptions.None);
+        /// <summary>
+        /// <para>
+        /// Method to run an automated Android build from the command line.
+        /// The build output is an Android .apk file located at
+        /// `build/piglet-viewer.apk' under the project root.
+        /// </para>
+        /// <para>
+        /// To run the build from the command line, switch to the
+        /// project root folder and run:
+        /// </para>
+        /// <para>
+        /// Unity.exe -logFile - -executeMethod
+        /// PigletViewer.AndroidBuilder.Build -batchmode -quit
+        /// </para>
+        /// </summary>
+        [MenuItem("PigletViewer/Build/Android")]
+        public static void Build()
+        {
+            const string buildPath = "build/piglet-viewer.apk";
+
+            // Android builds will error out until you change
+            // the package name from the default value.
+
+            PlayerSettings.SetApplicationIdentifier(
+                BuildTargetGroup.Android, "com.awesomesauce.piglet");
+
+            // Run the build.
+
+            var buildOptions = new BuildPlayerOptions
+            {
+                target = BuildTarget.Android,
+                locationPathName = buildPath,
+                scenes = new [] { "Assets/PigletViewer/Scenes/MainScene.unity" }
+            };
+
+            BuildPipeline.BuildPlayer(buildOptions);
+
+            Debug.Log($"Successfully built Android APK file at {buildPath}!");
         }
     }
 }
